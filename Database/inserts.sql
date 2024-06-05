cf17753dcab4175849e6c5c7f2cc942b78f996d1
Diegao79 <166340173+Diegao79@users.noreply.github.com>
2024-06-05 20:28:37 -0300

    Add files via upload

---
+++ Database/inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO Lojas_fornecedoras VALUES(1,'Fornecedora Teste','Rua Teste,4477','499956874','te4ste@lojateste.com','9999499999');
+INSERT INTO Produto VALUES(1,'Jogo Teste','Jogo para testar jogos',	'teste', 14.99, 'Teste jogos');
+INSERT INTO Catalogo_de_produtos VALUES(1,1,1, 14.99, '2024-06-02');
+INSERT INTO Estoque VALUES(1,1,1, 5);
+INSERT INTO Cliente_final VALUES(1,'Cliente teste','Qualquer coisa teste','49494949', 'testteste@teste.com', '458487576-84');
+INSERT INTO Pedido VALUES(1,1,'2024-06-02','ativo');
+INSERT INTO Faturamento VALUES(1,1,14.99,'2024-06-02');
+INSERT INTO entrega VALUES(1,1,'2024-06-02','2024-06-10','enviado','sedex');
+
+Select * from entrega;
+
