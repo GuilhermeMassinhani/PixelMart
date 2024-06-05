0044195c96453e3f03adf34829575e2ebb794fdc
Guilherme Massinhani de Souza <128001387+GuilhermeMassinhani@users.noreply.github.com>
2024-06-05 20:56:55 -0300

    Update inserts.sql

--- Database/inserts.sql
+++ Database/inserts.sql
@@ -4,10 +4,11 @@ INSERT INTO Catalogo_de_produtos VALUES(1,1,1, 14.99, '2024-06-02');
 INSERT INTO Estoque VALUES(1,1,1, 5);
 INSERT INTO Cliente_final VALUES(1,'Cliente teste','Qualquer coisa teste','49494949', 'testteste@teste.com', '458487576-84');
 INSERT INTO Pedido VALUES(1,1,'2024-06-02','ativo');
+INSERT INTO Pedido VALUES(2,1,'2024-06-03','ativo');
 INSERT INTO Faturamento VALUES(1,1,14.99,'2024-06-02');
 INSERT INTO entrega VALUES(1,1,'2024-06-02','2024-06-10','enviado','sedex');
+INSERT INTO entrega VALUES(2, 1, '2024-06-03', '2024-06-11', 'pendente', 'PAC');
 INSERT INTO entrega VALUES(3, 1, '2024-06-04', '2024-06-12', 'entregue', 'sedex');
-INSERT INTO entrega VALUES(4, 2, '2024-06-05', '2024-06-13', 'enviado', 'expresso');
 
 Select * from entrega;
 
